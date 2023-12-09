62df610150c115132842cbc596d52ef51b6d2f6a
raeganamar <121866101+raeganamar@users.noreply.github.com>
2023-12-09 20:34:15 +0700

    review_lazada

---
+++ analisis_review_produk.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+def main() :
+    st.title("Klasifikasi Sentimen Review Produk (Studi Kasus Lazada Dataset)")
+    menu_options = {
+     "Home" :":house:",
+     "Dataset":":book:",
+     "Model":":gear:",
+     "Analisis Deskriptif ":":bar_charts:",
+     "About":":ballon:"
+     }
+
+
+    
+
+if __name__ == "__main__":
+    main()
